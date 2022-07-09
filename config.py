b68ef8f7a9d91ea1557021f62b2ac1e0465c8c8a
Selfie SD <webtamil0@gmail.com>
2022-07-09 07:14:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class Config(object):
     
     OWNER_ID = int(os.environ.get("OWNER_ID", None)) # Your(owner's) telegram id
     
-    MONGO_STR = os.environ.get("MONGO_STR", "") # Get from MongoDB Atlas
+    MONGO_STR = os.environ.get("MONGO_STR", "mongodb+srv://erichdaniken:erichdaniken@cluster0.c13qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") # Get from MongoDB Atlas
 
     DOWNLOAD_LOCATION = "app//DOWNLOADS//" # The download location for users. (Don't change anything in this field!)
