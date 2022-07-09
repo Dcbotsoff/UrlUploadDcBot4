a605770016c640be8b502978c3c23b7a0511b2da
Selfie SD <webtamil0@gmail.com>
2022-07-09 07:12:43 +0530

    Update botMessages.py

--- helper/botMessages.py
+++ helper/botMessages.py
@@ -3,9 +3,9 @@
 
 # Bot defined Messages
 class BotMessage(object):
-    common_text = "\n\n<u>If you are facing any problem, so report at @AJPyroVerseGroup</u>"
+    common_text = "\n\n<u>If you are facing any problem, so report at @groupdc</u>"
     help_msg = f"<i>To use me, Just Send me any direct downloading link, and I will send you the file as telegram file.</i>{common_text}"
-    start_msg = f"<b>Hi, I am URL_UploaderBot Created by @AJPyroVerse and My Developer is @AJTimePyro.</b>\n{help_msg}"
+    start_msg = f"<b>Hi, I am URL_UploaderBot Created by @Groupdcbots and My Developer is @selfiebd.</b>\n{help_msg}"
     not_joined_community = f"<b>To use this bot, you need to Join our Channel and Group.</b>{common_text}"
     broadcast_failed = "<b>Broadcasting Message can`t be empty</b>"
     processing_url = "<i>Please wait while I am Processing File</i>"
